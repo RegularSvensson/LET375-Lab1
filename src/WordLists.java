1121ee80c8e7f734e812cdfdd2e06960b91f703f
RegularSvensson <elias.svensson.1992@gmail.com>
2016-03-31 17:49:33 +0200

    import Scanner

--- src/WordLists.java
+++ src/WordLists.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
