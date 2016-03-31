c69cd13c1b5ad10fbce194e7ca0574f68f2da26d
RegularSvensson <elias.svensson.1992@gmail.com>
2016-03-31 17:49:27 +0200

    import Map

--- src/WordLists.java
+++ src/WordLists.java
@@ -3,6 +3,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
