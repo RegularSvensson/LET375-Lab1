023ba51a9890111a9b41d6297c862b321946c1fb
RegularSvensson <elias.svensson.1992@gmail.com>
2016-03-31 18:31:11 +0200

    import TreeSet

--- src/WordLists.java
+++ src/WordLists.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
