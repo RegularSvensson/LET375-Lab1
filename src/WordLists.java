09cd5261e237f7e3b1c8e8a5b72277db9e0ba892
RegularSvensson <elias.svensson.1992@gmail.com>
2016-03-31 18:31:05 +0200

    Import Set

--- src/WordLists.java
+++ src/WordLists.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 import java.io.BufferedReader;
 import java.io.File;
